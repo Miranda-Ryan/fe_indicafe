720476f792c5d1fefd61cd80e2c4319c683c4072
Ryan Miranda <miranda.juderyan@gmail.com>
2022-04-01 16:53:13 +0530

    Added Auth0

--- src/components/Router/Router.js
+++ src/components/Router/Router.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "../LandingPage/LandingPage";
 import Catalog from "../Catalog/Catalog";
 import Cart from "../Cart/Cart";
+import Signin from "../Auth/Signin/Signin";
+import Signup from "../Auth/Signup/Signup";
+import Signout from "../Auth/Signout/Signout";
 
 const Router = () => {
   const deployRoutes = () => (
@@ -11,6 +14,9 @@ const Router = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/catalog" element={<Catalog />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signout" element={<Signout />} />
       </Routes>
     </BrowserRouter>
   );
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { AUTH0_DOMAIN, AUTH0_CLIENTID } from "./config/Config";
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+  <Auth0Provider
+    domain={AUTH0_DOMAIN}
+    clientId={AUTH0_CLIENTID}
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
 );
-
